f4a40fe2615e3d16a97bede969b5b58f1d09394e
Donavon Buchanan <5569820+donavoncade@users.noreply.github.com>
2020-09-12 03:20:02 -0400

    September 12 2020

--- Sources/SFSymbolsPackage/SFSymbolsPackage.swift
+++
@@ -1,3 +0,0 @@
-struct SFSymbolsPackage {
-    var text = "Hello, World!"
-}
--- Tests/SFSymbolsPackageTests/SFSymbolsPackageTests.swift
+++ Tests/SFSymbolsPackageTests/SFSymbolsPackageTests.swift
@@ -6,7 +6,7 @@ final class SFSymbolsPackageTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(SFSymbolsPackage().text, "Hello, World!")
+        XCTAssertEqual(SFSymbols.allCases.count, 2387)
     }
 
     static var allTests = [
